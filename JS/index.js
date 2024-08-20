1cf35da2b7058d6abbbab3c80cdce2049551d1a0
marypaz-flores <amarypaz.flores@gmail.com>
2024-08-20 17:47:06 +0200

    feat: Improve UX

--- JS/index.js
+++ JS/index.js
@@ -19,6 +19,10 @@ function generatePoem(event) {
     "You are an expert in writing romantic poems. Your task is to create exactly 4 lines of poetry following the user instructions below. Each line should be separated using <br>. The poem should be presented in basic HTML with no title, and the signature 'SheCodes AI' should appear on a separate line at the end inside a <strong> element. Do not count the signature as one of the 4 lines of the poem.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem for you about ${instructionsInput.value} </div>`;
+
   axios.get(apiURL).then(displayPoem);
 }
 
