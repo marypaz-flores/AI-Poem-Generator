efa8daaf6f9dbaf0418866cfb238f8d67cb4207e
marypaz-flores <amarypaz.flores@gmail.com>
2024-08-20 14:22:57 +0200

    feat: AI integration

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,25 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: ["Generating poem"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  //API URL
+  let apiKey = "19e9ee9138td443ob800fc47e41a5774";
+  let prompt = `Generate a poem about ${instructionsInput.value} in the same language of the word. Ensure that the language of the poem matches the language of the input. If the input is in Spanish, write in Spanish; if it's in German, write in German, and so on.`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate always a 4 line poem in basic HTML. Make sure to follow the user instructions below. No tittle should be displayed. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
