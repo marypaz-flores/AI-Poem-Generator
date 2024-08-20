15c1ee3de713bcebbcf2380e85093a59ec113bc3
marypaz-flores <amarypaz.flores@gmail.com>
2024-08-20 14:44:42 +0200

    feat: Update style

--- JS/index.js
+++ JS/index.js
@@ -14,9 +14,9 @@ function generatePoem(event) {
 
   //API URL
   let apiKey = "19e9ee9138td443ob800fc47e41a5774";
-  let prompt = `Generate a poem about ${instructionsInput.value} in the same language of the word. Ensure that the language of the poem matches the language of the input. If the input is in Spanish, write in Spanish; if it's in German, write in German, and so on.`;
+  let prompt = `Generate a poem about ${instructionsInput.value} in the language most commonly associated with that word. For example, if the word is in German, write the poem in German; if it’s in French, write it in French; if it’s in English, write it in English, and so on. Ensure that the entire poem is consistent in the identified language.`;
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate always a 4 line poem in basic HTML. Make sure to follow the user instructions below. No tittle should be displayed. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
+    "You are an expert in writing romantic poems. Your task is to create exactly 4 lines of poetry following the user instructions below. Each line should be separated using <br>. The poem should be presented in basic HTML with no title, and the signature 'SheCodes AI' should appear on a separate line at the end inside a <strong> element. Do not count the signature as one of the 4 lines of the poem.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiURL).then(displayPoem);
